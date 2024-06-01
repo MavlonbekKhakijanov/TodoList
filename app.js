e9d80999627b085677ca98bc47f6724b404e7524
MavlonbekKhakijanov <khakijanovamor@gmail.com>
2024-06-01 10:51:46 +0300

    todo time add

--- app.js
+++ app.js
@@ -8,27 +8,35 @@ const activeFilter = document.querySelector("#active");
 const completedFilter = document.querySelector("#completed");
 const deleteCompleted = document.querySelector("#delete-completed");
 let itemCount = 0;
-let now = new Date();
-let today = now.getDate() < 10 ? "0" + now.getDate() : now.getDate();
-let year = now.getFullYear();
-let hour = now.getHours() < 10 ? "0" + now.getHours() : now.getHours();
-let minute = now.getMinutes() < 10 ? "0" + now.getMinutes() : now.getMinutes();
-let months = [
-  "Yanvar",
-  "Fevral",
-  "Mart",
-  "Aprel",
-  "May",
-  "Iyun",
-  "Iyul",
-  "Avgust",
-  "Sentyabr",
-  "Oktyabr",
-  "Noyabr",
-  "Dekabr",
-];
 
-let data = `${today}. ${months[now.getMonth()]}. ${year} ${hour}:${minute}`;
+const getDate = () => {
+  let now = new Date();
+  let today = now.getDate() < 10 ? "0" + now.getDate() : now.getDate();
+  let year = now.getFullYear();
+  let hour = now.getHours() < 10 ? "0" + now.getHours() : now.getHours();
+  let minute =
+    now.getMinutes() < 10 ? "0" + now.getMinutes() : now.getMinutes();
+  let months = [
+    "Yanvar",
+    "Fevral",
+    "Mart",
+    "Aprel",
+    "May",
+    "Iyun",
+    "Iyul",
+    "Avgust",
+    "Sentyabr",
+    "Oktyabr",
+    "Noyabr",
+    "Dekabr",
+  ];
+  let counyerTime = `${today}.${
+    months[now.getMonth()]
+  }. ${year}, ${hour} : ${minute}`;
+  return counyerTime;
+};
+
+// let data = `${today}. ${months[now.getMonth()]}. ${year} ${hour}:${minute}`;
 formEl.addEventListener("submit", (e) => {
   e.preventDefault();
   if (!formEl.input.value.trim()) {
@@ -40,6 +48,7 @@ formEl.addEventListener("submit", (e) => {
     const paragrfText = document.createElement("p");
     const deleteIcon = document.createElement("i");
     const nowData = document.createElement("span");
+    nowData.textContent = getDate();
 
     // Klasslarni qo'shish
     icon.setAttribute("class", "icon-check fa-solid fa-check");
@@ -47,9 +56,6 @@ formEl.addEventListener("submit", (e) => {
     nowData.setAttribute("class", "data");
     spanRound.classList.add("spancha");
     li.classList.add("li-style");
-    nowData.textContent = `${today}. ${
-      months[now.getMonth()]
-    }. ${year} ${now.getHours()}:${now.getMinutes()}`;
 
     paragrfText.textContent = formEl.input.value;
 
@@ -71,6 +77,18 @@ formEl.addEventListener("submit", (e) => {
 
     formEl.input.value = "";
 
+    spanRound.addEventListener("click", () => {
+      li.classList.toggle("completed");
+
+      if (li.classList.contains("completed")) {
+        spanRound.appendChild(icon);
+        spanRound.classList.add("linear-gradient");
+      } else {
+        spanRound.classList.remove("linear-gradient");
+        spanRound.appendChild(icon).remove(icon);
+      }
+    });
+
     li.addEventListener("dblclick", () => {
       li.classList.toggle("completed");
 
@@ -84,6 +102,8 @@ formEl.addEventListener("submit", (e) => {
     });
     deleteIcon.addEventListener("click", (e) => {
       e.target.parentElement.remove();
+      itemCount--;
+      number.textContent = itemCount;
     });
   }
 });
